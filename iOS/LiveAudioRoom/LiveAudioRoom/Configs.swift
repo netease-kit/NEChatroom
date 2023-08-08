09f19f76099aab9f2a4da469d062bb2421583564
Ginger <jinjie03@corp.netease.com>
2023-08-08 17:48:20 +0800

    change: 删除不必要的模块

--- iOS/LiveAudioRoom/LiveAudioRoom/Configs.swift
+++ iOS/LiveAudioRoom/LiveAudioRoom/Configs.swift
@@ -5,93 +5,29 @@
 import Foundation
 import UIKit
 
-enum NPTConfig {
-  case online
-  case worldwide
-}
-
 enum Configs {
-  static var config: NPTConfig {
-    get {
-      if let config = UserDefaults.standard.string(forKey: "NPTConfig") {
-        switch config {
-        case "online":
-            return .online
-        case "worldwide":
-            return .worldwide
-        default:
-            return .online
-        }
-      } else {
-        UserDefaults.standard.setValue("online", forKey: "NPTConfig")
-        UserDefaults.standard.synchronize()
-      }
-        return .online
-    }
-    set {
-      switch newValue {
-      case .online:
-        UserDefaults.standard.setValue("online", forKey: "NPTConfig")
-        UserDefaults.standard.synchronize()
-      case .worldwide:
-        UserDefaults.standard.setValue("worldwide", forKey: "NPTConfig")
-        UserDefaults.standard.synchronize()
-      }
-    }
-  }
-
   static var extras: [String: String] {
-    switch config {
-    case .online:
-      return [String: String]()
-    case .worldwide:
-      return ["serverUrl": "oversea"]
-    }
+    IS_OVERSEA ? ["serverUrl": "oversea"] : [String: String]()
   }
 
   static var AppKey: String {
-    switch config {
-    case .online:
-      return APP_KEY_MAINLAND
-    case .worldwide:
-      return APP_KEY_OVERSEA
-    }
+    APP_KEY
   }
 
   static var AppSecret: String {
-    switch config {
-    case .online:
-      return APP_SECRET_MAINLAND
-    case .worldwide:
-      return APP_SECRET_OVERSEA
-    }
+    APP_SECRET
   }
 
   static var voiceRoomBaseUrl: String {
-    switch config {
-    case .online:
-      return kApiHost
-    case .worldwide:
-      return kOverSeaApiHost
-    }
+    IS_OVERSEA ? BASE_URL_OVERSEA : BASE_URL
   }
 
   static var orderSongBaseUrl: String {
-    switch config {
-    case .online:
-      return kApiHost
-    case .worldwide:
-      return kOverSeaApiHost
-    }
+    IS_OVERSEA ? BASE_URL_OVERSEA : BASE_URL
   }
 
   static var loginSampleBaseUrl: String {
-    switch config {
-    case .online:
-      return kApiHost
-    case .worldwide:
-      return kOverSeaApiHost
-    }
+    IS_OVERSEA ? BASE_URL_OVERSEA : BASE_URL
   }
 
   static var privacyUrl: String {
@@ -101,20 +37,10 @@ enum Configs {
   static var termsUrl: String {
     UIDevice.isChinese ? "https://yunxin.163.com/m/clauses/user" : "https://commsease.com/en/m/clauses/user"
   }
-
-  static var isSupportAIGC: Bool {
-    get {
-      // bool没有空值，无法区分未设值与设值为NO的情况，所以用string
-      if let isSupport = UserDefaults.standard.string(forKey: "NPTSupportAIGC"),
-         isSupport == "NO" {
-        return false
-      } else {
-        return true
-      }
-    }
-    set {
-      UserDefaults.standard.setValue(newValue ? "YES" : "NO", forKey: "NPTSupportAIGC")
-      UserDefaults.standard.synchronize()
-    }
-  }
 }
+
+// MARK: 全局变量，无需填写
+var accountId: String = ""
+var accessToken: String = ""
+var nickName: String = ""
+var avatar: String = ""
